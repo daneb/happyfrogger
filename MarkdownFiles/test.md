85db45cc974f4cd4a7ab1f826507cc27611ee74e
Dane Balia <daneb@tuta.io>
2024-01-04 21:23:27 +0200

    Base implementation for converting markdown to html

---
+++ MarkdownFiles/test.md
@@ -0,0 +1,9 @@
+# Hello World
+
+## Mulan
+
+* Hello
+* What 
+* Is
+* Your 
+* Name
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Markdig;
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var markdownFiles = Directory.GetFiles("MarkdownFiles", "*.md");
+
+        foreach (var file in markdownFiles)
+        {
+            string markdown = File.ReadAllText(file);
+            string html = Markdown.ToHtml(markdown);
+
+            // Optionally, insert 'html' into a template here
+
+            string outputFilename = Path.Combine("Output", Path.GetFileNameWithoutExtension(file) + ".html");
+            File.WriteAllText(outputFilename, html);
+        }
+
+        Console.WriteLine("Conversion complete.");
+    }
+}
